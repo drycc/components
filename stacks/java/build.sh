2363a8216f5760e68623fe8b0cb2971a5395e869
duanhongyi <duanhongyi@doopai.com>
2023-06-15 16:34:21 +0800

    chore(stacks): change java to long time support

--- stacks/java/build.sh
+++ stacks/java/build.sh
@@ -3,10 +3,22 @@
 # Load stack utils
 . /usr/bin/stack-utils
 
+init_arch() {
+  case $(uname -m) in
+    aarch64) url="https://aka.ms/download-jdk/microsoft-jdk-${STACK_VERSION}-linux-aarch64.tar.gz";;
+    x86_64) url="https://aka.ms/download-jdk/microsoft-jdk-${STACK_VERSION}-linux-x64.tar.gz";;
+  esac
+}
+
 # Implement build function
 function build() {
   generate-stack-path
-  ./make.sh
+  case $(uname -m) in
+    aarch64) url="https://aka.ms/download-jdk/microsoft-jdk-${STACK_VERSION}-linux-aarch64.tar.gz";;
+    x86_64) url="https://aka.ms/download-jdk/microsoft-jdk-${STACK_VERSION}-linux-x64.tar.gz";;
+  esac
+  curl -sSL "${url}" | tar -xz
+  cp -rf jdk-${STACK_VERSION}*/* "${DATA_DIR}" && rm -rf jdk-${STACK_VERSION}*
   mkdir -p "${DATA_DIR}"/env
   echo "/opt/drycc/java" > "${DATA_DIR}"/env/JAVA_HOME
   cat  << EOF >> ${PROFILE_DIR}/${STACK_NAME}.sh
